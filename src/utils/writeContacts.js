382e3ac8c9b42c6db1ab87811bdddba53599f6cf
Victoria <vika96karnauhova@gmail.com>
2024-10-17 23:13:43 +0300

    done hw1

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,5 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { writeFile } from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = (contacts) =>
+  writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
